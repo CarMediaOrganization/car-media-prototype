94a461a55fd218c3a9f16083beb4ea91567294ed
ChristianGeorgiev <kristian.v.georgiev@gmail.com>
2018-04-28 00:56:38 +0300

    Project structure and package changes

--- src/main/java/com/somecompany/carmediaprototype/controllers/IndexController.java
+++ src/main/java/com/somecompany/carmediaprototype/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.somecompany.carmediaprototype;
+package com.somecompany.carmediaprototype.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
